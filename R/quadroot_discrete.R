dbd9c6d878238a5d80eb4a8f40c51bf5f78987e3
nathaniel-morgan <nathaniel.morgan1999@gmail.com>
2024-05-28 15:14:34 -0500

    setting up for discrete quadroot function

---
+++ R/quadroot_discrete.R
@@ -0,0 +1,24 @@
+#' Find points on boundary of discrete classification
+#' 
+#'
+#' @section Arguments: `quadroot_discrete()` understands the following arguments:
+#'
+#'   - x - The region to be searched
+#'
+#' @name quadroot_discrete
+#'
+#'
+#' @examples
+#' # Basic test
+#' 
+#' set.seed(17)
+#' 1+1
+#' 
+
+#' @rdname quadroot_discrete
+#' @format NULL
+#' @usage NULL
+#' @export
+quadroot_discrete <- function(x){
+  1+1
+}
